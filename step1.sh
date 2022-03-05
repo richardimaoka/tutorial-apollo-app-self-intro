3fee685be9f451ffdc92076bc48a8847d807d864
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-03-06 00:07:41 +0900

    updated steps

--- step1.sh
+++ step1.sh
@@ -5,6 +5,6 @@
 # :large_orange_diamond: Action: ターミナルで以下の一連のコマンドを入力してください
 #
 # ```
-cp answers/html/index1.html index.html
-cp answers/html/profile.png profile.png
+git clone https://github.com/richardimaoka/tutorial-apollo-app-self-intro.git
+cd tutorial-apollo-app-self-intro
 # ```
---
+++ step3-1.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp -r answers/file-server file-server
+cd file-server
+npm run start
