313b5f1d7fffd8ed6e823896e901ef50187353f6
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-03-12 00:25:33 +0900

    Add step 7

--- steps/step7.sh
+++ steps/step7.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-# ## GraphQLサーバー
+## Reactクライアントで、GraphQLサーバーから取得したレスポンスを使う
 
+# :large_orange_diamond: Action: 以下のコマンドを実行してください
+
+# ```terminal: メイン (ターミナル)
 cp answers/client3/* client
+# ```
+
+## Bonus: エラーハンドリングを入れてみる
+
