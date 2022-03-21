e73d5d77da208a977766b909493e17a9da3f4582
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-03-21 15:58:45 +0900

    restore step 1-3

--- steps/step1-3.sh
+++ steps/step1-3.sh
@@ -2,3 +2,18 @@
 
 cd "$(dirname "$0")" || exit # REMOVE THIS IN aggregate.sh 
 cd ../ || exit               # REMOVE THIS IN aggregate.sh - cd to the git repository root
+
+# :large_orange_diamond: Action: ファイルサーバーは走らせたまま、メインのターミナルで以下のコマンドを実行してください
+
+# ```terminal: メイン (ターミナル)
+cp answers/step1-html/index2.html index.html
+rm profile.png
+# ```
+
+# :large_orange_diamond: Action: 先程のブラウザ画面をリロードしてください
+
+# :white_check_mark: Result: 同じ画面が表示されればOKです。
+
+# ![2022-03-03_08h29_32.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/75738/bb743a21-9177-6d7f-74d3-1d187a5e97c8.png)
+
+# `profile.png`がローカルファイルではなく、ファイルサーバーの http://localhost:8080/images/profile.png から取得されています。
